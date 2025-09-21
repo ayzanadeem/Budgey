d1d8d4df825bd1f9c926177e8fcdad8147b8a8ab
Ayza Nadeem <a.nadeem@rewaatech.com>
2025-09-21 18:50:37 +0500

    firebase and google services + hilt modules

--- app/src/main/java/com/example/budgey/BudgeyApplication.kt
+++ app/src/main/java/com/example/budgey/BudgeyApplication.kt
@@ -4,4 +4,11 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BudgeyApplication : Application()
+class BudgeyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // Firebase is automatically initialized through google-services plugin
+        // Additional app-level initialization can be added here if needed
+    }
+}
